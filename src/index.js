4332d63d0f6b51885d4c1b8a3f8b694eafd25187
Fred <leferpz@gmail.com>
2024-06-09 12:57:55 -0400

    Now separates generic data to daily weather data

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const DOM = {
     $locationChoice: document.querySelector("#location-choice"),
     $goButton: document.querySelector("#go"),
     $errorMessage: document.querySelector(".error-message"),
+    $weatherLocation: document.querySelector(".location"),
+    $weatherCountry: document.querySelector(".country"),
+    $weatherLocalTime: document.querySelector(".local-time"),
     $day0: document.querySelector(".data-output1"),
     $day1: document.querySelector(".data-output2"),
     $day2: document.querySelector(".data-output3"),
@@ -17,6 +20,7 @@ DOM.$goButton.addEventListener("click", () => {
 })
 
 async function runWeather(locationValue) {
+
     try {
     let weatherData = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=bab5dec6e8544d13822222523240406&q=${locationValue}&days=5`, {mode: 'cors'});
     let usableData = await weatherData.json();
@@ -26,19 +30,21 @@ async function runWeather(locationValue) {
     let country = usableData.location.country;
     let localtime = usableData.location.localtime;
 
+    console.log(location, country, localtime);
+
+    DOM.$weatherLocation.innerHTML = location;
+    DOM.$weatherCountry.innerHTML = country;
+    DOM.$weatherLocalTime.innerHTML = localtime;
+
     for (let i = 0; i <= 4; i++) {
 
         let temp = usableData.forecast.forecastday[i].day.avgtemp_c;
         let condition = usableData.forecast.forecastday[i].day.condition.text;
-
         let date = usableData.forecast.forecastday[i].date;
 
         console.log(location, country, localtime, date, temp, condition);
 
         DOM[`$day${i}`].innerHTML = `
-        Location: ${location} <br>
-        Country: ${country} <br>
-        Local Time: ${localtime} <br>
         Date: ${date} <br>
         Temperature (°C): ${temp} <br>
         Conditions: ${condition}`
