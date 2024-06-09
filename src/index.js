05a8b10800c98525c33e9102e81cc2e77ddfc527
Fred <leferpz@gmail.com>
2024-06-09 10:28:54 -0400

    Now fully working with basic display on the DOM and error messages for wrong input

--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
 import "./style.css";
 
 const DOM = {
-    $location: document.querySelector("#location"),
-    $goButton: document.querySelector("#go")
+    $locationChoice: document.querySelector("#location-choice"),
+    $goButton: document.querySelector("#go"),
+    $dataOutput: document.querySelector(".data-output")
 }
 
-console.log("hi");
-
 DOM.$goButton.addEventListener("click", () => {
-    let locationValue = DOM.$location.value;
+    let locationValue = DOM.$locationChoice.value;
     runWeather(locationValue);
 })
 
 async function runWeather(locationValue) {
-    let weatherdata = await fetch(`http://api.weatherapi.com/v1/current.json?key=bab5dec6e8544d13822222523240406&q=${locationValue}`, {mode: 'cors'});
-    console.log(weatherdata);
-    return weatherdata;
+    try {
+    let weatherData = await fetch(`http://api.weatherapi.com/v1/current.json?key=bab5dec6e8544d13822222523240406&q=${locationValue}`, {mode: 'cors'});
+    let usableData = await weatherData.json();
+    console.log(usableData);
+
+    let location = usableData.location.name;
+    let country = usableData.location.country;
+    let time = usableData.location.localtime;
+    let temp = usableData.current.temp_c;
+    let condition = usableData.current.condition.text;
+
+
+    DOM.$dataOutput.innerHTML =   `Location: ${location} <br>
+                            Country: ${country} <br>
+                            Local Time: ${time} <br>
+                            Temperature (°C): ${temp} <br>
+                            Conditions: ${condition}`;
+    } catch (error) {
+        DOM.$dataOutput.innerHTML = "Please input a real place"
+    }
 }
 
